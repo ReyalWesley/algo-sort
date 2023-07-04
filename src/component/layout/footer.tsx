390ec507a8e7f634cbd0ea9fa1a27cbd9dec3e70
ReyalWesley <reyal.wesley@gmail.com>
2023-07-04 21:39:45 +0200

    feat(layout): footer component

---
+++ src/component/layout/footer.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const Footer: React.FC = () => {
+  return (
+    <footer className="footer footer-center p-4 bg-rose-50 text-base-content">
+      <div>
+        <p>Copyright © 2023 - All right reserved by zekave</p>
+      </div>
+    </footer>
+  );
+};
